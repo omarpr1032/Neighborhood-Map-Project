f94cec22c7e88bdc57812d6c439c6563159b2a0a
omarpr1032 <omarpr1032@gmail.com>
2018-12-22 09:52:13 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,118 @@
+// Global Variables
+var sidebarCollapsed = false;
+var foursquareClientID = "EJUVUP3N1RXOYF33BRRLUSA0W5EDY5ZQWOLOAJWIALKOZKJF";
+var foursquareClientSecret = "XQRIRGFFFM22ACACPMTL2HDYHCZXUJ4SNY5KPLOOSFQPR5TL";
+var map;
+
+
+// Sidebar Collapse Toggle
+function ToggleSidebar(){
+	// Get the sidebar element
+  	var sidebar = document.getElementById("sidebar");
+
+	// Sidebar Collapse
+	if(sidebarCollapsed == true){
+		sidebar.style.marginLeft=0;
+		sidebarCollapsed = false;
+	}else{
+		sidebar.style.marginLeft="-260px";
+		sidebarCollapsed = true;
+	}
+}
+
+// Knockout Application View Model
+var AppViewModel = function (){
+	var self = this;
+	this.markers = [];
+	
+	// Populate the Info Window with Foursquare API Address Content
+	this.showInfoWindow = function(){
+		var marker = this;
+		var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?ll=' + marker.lat + ',' + marker.lng + 
+			'&client_id=' + foursquareClientID +'&client_secret=' + foursquareClientSecret + '&query=' + marker.title + 
+			'&v=20181009' + '&m=foursquare';
+
+		$.getJSON(fourSquareUrl, function(result){
+			var response = result.response.venues[0];
+			self.street = response.location.formattedAddress[0];
+			self.city = response.location.formattedAddress[1];
+			self.zip = response.location.formattedAddress[3];
+
+			var htmlContentFoursquare ='<h6>' + marker.title +'</h6>' + '<div>' + '<h7> Address: </h7>' +'<p>' + self.street + '</p>' + '</div>';
+			
+			self.infoWindow.setContent(htmlContentFoursquare);
+			self.infoWindow.open(map,marker);
+		});
+	};
+
+	//Google Map Initialization
+	 this.initMap= function(){
+		// Get the div element for map
+		var mapDiv = document.getElementById('map');
+		 // Define the map options
+		var mapOptions = {
+			center: {lat:18.159092,lng:-66.095541},
+			zoom: 13
+		};
+
+		// Constructor creates a new map - only center and zoom are required.
+		var map = new google.maps.Map(mapDiv, mapOptions);
+
+		 // Create the map markers for each location
+		 for (var i =0; i< locations.length; i++){			
+			this.marker = new google.maps.Marker({
+				map: map,
+				position:{
+					lat: locations[i].lat,
+					lng: locations[i].lng
+				},
+				title: locations[i].title,
+				lat: locations[i].lat,
+				lng: locations[i].lng,
+				id: i,
+				animation: google.maps.Animation.DROP
+			});
+			
+		 	// Add to array of markers
+	 		this.markers.push(this.marker);
+			// Create the listeners for the markers
+			this.marker.addListener("click", self.showInfoWindow);
+		 };
+		 
+		 // Define the Info Window
+		 this.infoWindow = new google.maps.InfoWindow(); 
+	}
+
+	 // Load Map
+	this.initMap();
+	
+	// Search Filter Observable
+  	this.filter = ko.observable("");
+	
+	// Filter Computed Function
+	this.filteredLocations = ko.computed(function(){
+		var result = [];
+		for (var i=0; i < self.markers.length; i++){
+			var marker = self.markers[i];
+			// Filter and only show the filtered markers
+			if(marker.title.toLocaleLowerCase().includes(self.filter().toLowerCase())){
+				result.push(marker);
+				self.markers[i].setVisible(true);
+			}else{
+				self.markers[i].setVisible(false);
+			}
+		};
+		// Return the markers locations array
+		return result;
+	});
+};
+
+// Use knockout JS
+function startApp() {
+    ko.applyBindings(new AppViewModel());
+}
+
+
+
+
+
---
+++ js/locations.js
@@ -0,0 +1,17 @@
+var locations=[
+  {title: 'El Rubí', lat:18.154774981266215, lng:-66.09462592020726},
+  {title: 'Nonna Pizza A Leña', lat:18.158217, lng:-66.097077},
+  {title: 'Los Dos Mangoes', lat:18.168730732532847, lng:-66.11539899054037},
+  {title: 'e pura Cepa Restaurante', lat:18.156274268853316, lng:-66.09976682541563},
+  {title: 'Cultura Barra Artesanal', lat:18.13978568092452, lng:-66.08879903301975},
+  {title: 'Bruny s Rest', lat:18.151185800531557, lng:-66.09740839220099},
+  {title: 'Café Tertulías', lat:18.166433342980604, lng:-66.09839010318125},
+  {title: 'Café Prieto by Los Amigos', lat:18.151453324194737, lng:-66.10266267727032},
+  {title: 'Ferretería Papo s Commercial', lat:18.155765533447266, lng:-66.10005950927734},
+  {title: 'El Pinchorro', lat:18.13982202630564, lng:-66.10968282399209},
+  {title: 'La Mina De Oro', lat:18.168904020212327, lng:-66.09578770201315},
+  {title: 'Café Aromante', lat:18.156275, lng:-66.099899},
+  {title: 'Panadería Y Repostería Guavate', lat:18.142662, lng:-66.095416},
+  {title: 'Lechonera Los Amigos', lat:18.150333142091544, lng:-66.10314765324195},
+  {title: 'Cafetin El Puente', lat:18.163179, lng:-66.090682}
+];
